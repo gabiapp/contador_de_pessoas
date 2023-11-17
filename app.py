98611e8759557adeb40e11478a9268c4b1395730
Gabriela <gabrielaap.nunez@gmail.com>
2023-11-17 12:25:31 -0300

    O arquivo app.py teve sua estrutura modificada para melhor apresentação dos componentes apresentados na interface. A barra lateral foi reduzida a apenas apresentar uma selectbox para decidir entre as três câmeras disponíveis, quando uma ou outra é selecionada, na tela principal são exibidos tabs para apresentar isoladamente cada informação relacionada a câmera, como fluxo de visitantes, fluxo acumulado e visão em tempo real da câmera

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import random
 import matplotlib.pyplot as plt
 
 st.write('''
-         # Controle de Fluxo de Pessoas
-         ## Smart Pixel
-         ''')
+         # Controle de Fluxo de Pessoas - Smart Pixel
+         
+        ''')
 
 cols = ["11/05", "14/05", "17/05", "20/05", "23/05", "26/05", "29/05", "01/06", "04/06", "07/06", "09/06"]
 line = [0, 200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000]
@@ -19,52 +18,54 @@ x = df.columns
 y = df.index
 plt.plot(x,y)
 
-
-add_camera01 = st.sidebar.selectbox(
-    "Câmera 01",
-    ("Tempo real", "Fluxo acumulado", "Fluxo de visitantes"),
-    key="camera01"
+add_cameras = st.sidebar.selectbox(
+    "Câmeras",
+    ("Câmera 01", "Câmera 02", "Câmera 03")
 )
 
-if add_camera01 == 'Tempo real':
-    st.write("## Câmera 01 em tempo real")
-    st.video("VIDEO - PESSOAS ANDANDO NA RUA HD.mp4")
-elif add_camera01 == 'Fluxo acumulado':
-    st.write("## Fluxo acumulado obtido pela câmera 01")
-    st.pyplot(plt)
-else:
-    st.write("## Fluxo de visitantes obtido pela câmera 01")
-    st.pyplot(plt)
-
+if add_cameras == "Câmera 01":
+    a01, b01, c01 = st.tabs(["Fluxo acumulado", "Fluxo de visitantes", "Tempo Real"])
     
-add_camera02 = st.sidebar.selectbox(
-    "Câmera 02",
-    ("Tempo real", "Fluxo acumulado", "Fluxo de visitantes"),
-    key="camera02"
-)
+    with a01:
+        st.header("Fluxo acumulado obtido da câmera 01")
+        st.pyplot(plt)
 
-if add_camera02 == 'Tempo real':
-    st.write("## Câmera 02 em tempo real")
-    st.video("VIDEO - PESSOAS ANDANDO NA RUA HD.mp4")
-elif add_camera02 == 'Fluxo acumulado':
-    st.write("## Fluxo acumulado obtido pela câmera 02")
-    st.pyplot(plt)
-else:
-    st.write("## Fluxo de visitantes obtido pela câmera 02")
-    st.pyplot(plt)
+    with b01:
+        st.header("Fluxo de visitantes obtido da câmera 01")
+        st.pyplot(plt)
+    
+    with c01:
+        st.header("Imagens em tempo real da câmera 01")
+        st.video("video_modelo.mp4")
 
-add_camera03 = st.sidebar.selectbox(
-    "Câmera 03",
-    ("Tempo real", "Fluxo acumulado", "Fluxo de visitantes"),
-    key="camera03"
-)
 
-if add_camera03 == 'Tempo real':
-    st.write("# Câmera 03 em tempo real")
-    st.video("VIDEO - PESSOAS ANDANDO NA RUA HD.mp4")
-elif add_camera03 == 'Fluxo acumulado':
-    st.write(" ## Fluxo acumulado obtido pela câmera 03")
-    st.pyplot(plt)
-else:
-    st.write(" ## Fluxo de visitantes obtido pela câmera 03")
-    st.pyplot(plt)
+if add_cameras == "Câmera 02":
+    a02, b02, c02 = st.tabs(["Fluxo acumulado", "Fluxo de visitantes", "Tempo Real"])
+
+    with a02:
+        st.header("Fluxo acumulado obtido da câmera 02")
+        st.pyplot(plt)
+
+    with b02:
+        st.header("Fluxo de visitantes obtido da câmera 02")
+        st.pyplot(plt)
+    
+    with c02:
+        st.header("Imagens em tempo real da câmera 02")
+        st.video("video_modelo.mp4")
+
+
+if add_cameras == "Câmera 03":
+    a03, b03, c03 = st.tabs(["Fluxo acumulado", "Fluxo de visitantes", "Tempo Real"])
+
+    with a03:
+        st.header("Fluxo acumulado obtido da câmera 03")
+        st.pyplot(plt)
+
+    with b03:
+        st.header("Fluxo de visitantes obtido da câmera 03")
+        st.pyplot(plt)
+    
+    with c03:
+        st.header("Imagens em tempo real da câmera 03")
+        st.video("video_modelo.mp4")
